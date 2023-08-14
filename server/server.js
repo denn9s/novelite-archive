78032ca5fd8d3e3d53c263bbce369a0d235d9850
denn9s <cs.dennisong@gmail.com>
2023-08-13 20:46:07 -0700

    feat: add rate limiter

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const cron = require('node-cron');
 const cors = require('cors');
 const express = require('express');
+const rateLimit = require('express-rate-limit')
+
 const routes = require('./routes');
 const { updateDB } = require('./services/update_stories');
 const { login } = require('./services/scraper_login');
@@ -8,7 +10,15 @@ const { login } = require('./services/scraper_login');
 const app = express();
 const port = 6969;
 
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 1000, // 1000 request per window
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 app.use(cors());
+app.use(limiter);
 
 cron.schedule('59 23 * * *', function() {
     console.log('running scheduled job')
