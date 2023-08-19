4d7ceddd9da6205048fd39ecebcec7fbeda1258b
denn9s <cs.dennisong@gmail.com>
2023-08-19 03:40:57 -0700

    feat: add homepage design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,38 @@
+import { Link } from "react-router-dom";
+
+import '../styles/home.css';
+
 const Home = () => {
     return (
-        <div className="mt-[10em]">
-            <h1 className="text-2xl text-center text-white">Nothing here yet!</h1>
-            <p className="text-center text-light-gray text-xs">I'll make something soon...</p>
-        </div>
+        <>
+            <div className="text-center">
+                <h1 className="mt-5 text-5xl font-bold text-white">Novelites' Archive</h1>
+                <p className="text-center text-light-gray text-s">Welcome to the (unofficial) Shiorin archive!</p>
+            </div>
+            
+            <div className="relative justify-center flex mt-10 mx-6">
+                <div className="home-buttons-container">
+                        <Link to="/stories">
+                            <button className="whitespace-nowrap text-white purple-shadow p-1 rounded-lg vn-choice">
+                                "A story."
+                            </button>
+                        </Link>
+                        <div></div>
+                        <Link to="/stories/archive">
+                            <button className="whitespace-nowrap text-white purple-shadow p-1 my-1 rounded-lg vn-choice">
+                                "The story archive."
+                            </button>
+                        </Link>
+                    </div>
+                <img src="homepage-vn.png" alt="shiori dialogue"/>
+            </div>
+            
+            <div>
+                <p className="text-center text-light-gray text-xs mt-1">
+                    More to come soon(?)
+                </p>
+            </div>
+        </>
     )
 };
 
